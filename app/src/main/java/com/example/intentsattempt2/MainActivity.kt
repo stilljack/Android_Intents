793a448b4666052ee53a0375c71b21df391eac1a
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-08 14:40:44 -0700

    oh we real real close to done here

--- app/src/main/java/com/example/intentsattempt2/MainActivity.kt
+++ app/src/main/java/com/example/intentsattempt2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.ArrayList
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         var imageList: ArrayList<ImageData> = ArrayList()
         var index = 0
+        val INITIAL_SELECTION = 1
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,41 +25,44 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
             if (intent.resolveActivity(packageManager) != null) {
-                startActivityForResult(intent, 1)
+                startActivityForResult(intent, INITIAL_SELECTION)
             }
         }
 
-        fun createTextView(string: String?, int: Int): TextView {
-            val view = TextView(this)
-            view.tag = imageList[int]
-            view.text = string
-            view.textSize = 24f
+    }
+    fun createTextView(string: String?, int: Int): TextView {
+        val view = TextView(this)
+        view.tag = imageList[int]
+        view.text = string
+        view.textSize = 24f
 
-            view.setOnClickListener {
-                val intent = Intent(this, DetailsView::class.java)
-                intent.putExtra("picture", imageList[int].fileUri)
-                startActivity(intent)
-            }
-            return view
+        view.setOnClickListener {
+            val intent = Intent(this, DetailsView::class.java)
+            intent.putExtra("picture", imageList[int].fileUri)
+            startActivity(intent)
         }
+        return view
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
+        val fullPhotoUri: Uri? = data!!.data
+        Log.i("stage1","stage1")
+        if (requestCode == INITIAL_SELECTION && resultCode == RESULT_OK) {
+            imageList.add(index, ImageData(fullPhotoUri))
+            Log.i("stage2","stage2")
+            picture_linear_layout.addView(
+                createTextView(imageList[index].fileUriString, index++)
+            )
+        } else if (requestCode == 2 && resultCode == RESULT_OK) {
+            // Make sure the request was successful
+            val returnedData = data.getSerializableExtra("object") as ImageData
 
-        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-
-            val fullPhotoUri: Uri? = data!!.data
-
-            if (requestCode == 1 && resultCode == RESULT_OK) {
-                imageList.add(ImageData(fullPhotoUri))
-                picture_linear_layout.addView(
-                    createTextView(fullPhotoUri.toString(), index++)
-                )
-            } else if (requestCode == 2T && resultCode == RESULT_OK) {
-                // Make sure the request was successful
-                val returnedData = data.getSerializableExtra("object") as ImageData
-
-                for (i in 0..imageList.size) {
-                    if (imageList[i].fileUriString == returnedData.fileUriString) {
-                        imageList[i] = returnedData
-                    }
+            for (i in 0..imageList.size) {
+                if (imageList[i].fileUriString == returnedData.fileUriString) {
+                    imageList[i] = returnedData
                 }
             }
+        }
+    }
+
+}
