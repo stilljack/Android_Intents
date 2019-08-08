0e66fc3a2193e6510085ed9e1f4c4786de8e78d1
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-08 14:26:53 -0700

    next step

--- app/src/main/java/com/example/intentsattempt2/ImageData.kt
+++ app/src/main/java/com/example/intentsattempt2/ImageData.kt
@@ -1,9 +1,25 @@
 package com.example.intentsattempt2
 
+import android.net.Uri
 import java.io.Serializable
 import java.net.URI
 
-class ImageData (uri: URI): Serializable {
-    private val uri = uri
-    private val uriString = uri.toString()
+class ImageData (uri: Uri?): Serializable {
+    var name: String? = null
+    var fileUriString: String? = null
+    var description: String? = null
+    var fileUri: Uri?
+        get() = Uri.parse(fileUriString)
+    //gets raw URI string from FileUriStrong designates it a URI jss
+
+    init {
+        this.fileUri = uri
+        this.fileUriString = uri.toString()
+        //not going to pretend to understand in depth why this works but looks like a regular expression
+        //to snag the file name off the end of URI -- one day I'll have magic powers if i keep trying
+        val path = uri!!.path!!.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        this.name = path[path.size - 1]
+        //AFAICT this gets the second to last chunk of path to find the name of the file jss
+        this.description = "idk my bff jill"
+    }
 }
--- app/src/main/java/com/example/intentsattempt2/MainActivity.kt
+++ app/src/main/java/com/example/intentsattempt2/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.example.intentsattempt2
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var imageList: ArrayList<ImageData> = ArrayList()
+        var index = 0
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        add_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, 1)
+            }
+        }
+
+        fun createTextView(string: String?, int: Int): TextView {
+            val view = TextView(this)
+            view.tag = imageList[int]
+            view.text = string
+            view.textSize = 24f
+
+            view.setOnClickListener {
+                val intent = Intent(this, DetailsView::class.java)
+                intent.putExtra("picture", imageList[int].fileUri)
+                startActivity(intent)
+            }
+            return view
+        }
+
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+            val fullPhotoUri: Uri? = data!!.data
+
+            if (requestCode == 1 && resultCode == RESULT_OK) {
+                imageList.add(ImageData(fullPhotoUri))
+                picture_linear_layout.addView(
+                    createTextView(fullPhotoUri.toString(), index++)
+                )
+            } else if (requestCode == 2T && resultCode == RESULT_OK) {
+                // Make sure the request was successful
+                val returnedData = data.getSerializableExtra("object") as ImageData
+
+                for (i in 0..imageList.size) {
+                    if (imageList[i].fileUriString == returnedData.fileUriString) {
+                        imageList[i] = returnedData
+                    }
+                }
+            }
